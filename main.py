a0b8d1319971bd8365d9231405d18a305cfbadaa
Alex Coté <alx929@gmail.com>
2018-04-11 12:12:00 -0500

    Added transition conditions in each state.

--- main.py
+++ main.py
@@ -4,8 +4,21 @@
 # Alex Coté - alc552
 # Marc Moore - mnm419
 
+from finch import Finch
+
+finch = Finch()
+
 fStates = ["LightSeek", "ObstAvoid", "Rest", "WarmSeek"]
 curState = 0
+
+wantedLight = .7 # Temp Value: 0-1.0
+wantedTemp = 30 # Degrees in C
+wanderSpeedMax = .5
+wanderSpeedMin = .2
+rushSpeedMax = 1
+rushSpeedMin = .5
+curSpeedL = 0
+curSpeedR = 0
 
 def FinchRun():
 	global fStates
@@ -14,13 +27,43 @@ def FinchRun():
 
 	while (finchRunning):
 		if (fStates[curState] == "LightSeek"):
-			pass
+			# Move at rushing speed
+
+			# Check for obstacle
+			if (finch.obstacle):
+				curState = 1
+			elif (finch.light == wantedLight):
+				if (finch.temperature == wantedTemp):
+					curState = 2
+				else:
+					curState = 3
 		elif (fStates[curState] == "ObstAvoid"):
-			pass
+			# Turn to the right until obstacle is gone
+
+			if (finch.obstacle == False):
+				if (finch.light < wantedLight):
+					curState = 0
+				elif (finch.temperature < wantedTemp):
+					curState = 3
+				else: curState = 2
 		elif (fStates[curState] == "Rest"):
-			pass
+			if (finch.light < wantedLight):
+				curState = 0
+			elif (finch.temperature < wantedTemp):
+				curState = 3
+			
+			if (curState != 2):
+				if (finch.obstacle == True):
+					curState = 1
 		elif (fStaets[curState] == "WarmSeek"):
-			pass
+			# Wander until warmth has been found
+			
+			if (finch.obstacle == True):
+				curState = 1
+			elif (finch.light < wantedLight):
+				curState = 0
+			elif (finch.temperature >= wantedTemp):
+				curState = 2
 		else:
 			curState = 0
 
